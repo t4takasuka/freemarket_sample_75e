5e7022df58594c21e594472a4612b734da5254f0
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-05-21 17:42:33 +0900

    [add]sending_destinationsテーブル追加

---
+++ db/migrate/20200521064119_create_sending_destinations.rb
@@ -0,0 +1,19 @@
+class CreateSendingDestinations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sending_destinations do |t|
+      t.string :destination_first_name,       null: false
+      t.string :destination_family_name,      null: false
+      t.string :destination_first_name_kana,  null: false
+      t.string :destination_family_name_kana, null: false
+      t.integer :post_code,                   null: false
+      t.string :prefecture_code,              null: false
+      t.string :city,                         null: false
+      t.string :house_number,                 null: false
+      t.string :building_name
+      t.integer :phone_number                 null: false
+      t.references :user,                     null: false, foreign_key: true
+      t.timestamps
+    end
+    add_index :sending_destinations, :phone_number, unique: true
+  end
+end
