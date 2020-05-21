c9b409bede3035d1825fa78410e43e03e0f6181f
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-05-21 15:36:09 +0900

    [add]deviseインストール

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, if: :production?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def basic_auth
@@ -12,5 +13,9 @@ class ApplicationController < ActionController::Base
   def production?
     Rails.env.production?
   end
-
+  
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
