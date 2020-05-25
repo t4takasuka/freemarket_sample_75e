9158f1e76d9ab8ad9d949a1c81015eaf76ee662a
t4takasuka <t4botks@gmail.com>
2020-05-25 19:16:30 +0900

    [add]item-モデル、マイグレーションファイル追加
    - itemモデル追加
    - itemモデルマイグレーションファイル設定
    - gemファイルのactive_hashを追加
    - itemimgモデルマイグレーションを修正

--- db/migrate/20200522064538_create_itemimgs.rb
+++ db/migrate/20200522064538_create_itemimgs.rb
@@ -2,8 +2,7 @@ class CreateItemimgs < ActiveRecord::Migration[5.2]
   def change
     create_table :itemimgs do |t|
       t.text :image, null: false
-      #t.references :item, null: false, foreign_key: true
-      t.timestamps
+      t.references :item, null: false, foreign_key: true
     end
   end
 end
