a92b08bbe3c7390329336593ce3daacd95317da6
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-05-26 13:58:54 +0900

    [add]新規登録画面ビュー実装

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module FreemarketSample75e
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
-    config.action_view.field_error_proc = proc { |html_tag, _instance| html_tag }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
