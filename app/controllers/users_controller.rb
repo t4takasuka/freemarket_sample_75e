05bbc492855a3bf64fa6eebc201e3849ed0131a3
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-05-31 11:21:34 +0900

    [add] ログインしていないユーザーはマイページに行けないように修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :move_to_index, except: :show
+
   def show
     @user = User.find(params[:id])
   end
@@ -7,5 +9,11 @@ class UsersController < ApplicationController
   end
   
   def mypage
+  end
+
+  private
+
+  def move_to_index
+    redirect_to root_path unless user_signed_in?
   end
 end
