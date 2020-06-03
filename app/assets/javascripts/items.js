34bc2f042ab598a014af18562637da173351cff9
yohei-shimizu-1190 <yoheishimizu1190@yahoo.co.jp>
2020-06-03 18:23:56 +0900

    [add] file_field 横の削除ボタン位置変更

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -4,8 +4,8 @@ $(document).on('turbolinks:load', () => {
     const html = `<div data-index="${num}" class="js-file_group">
                     <input class="js-file" type="file"
                     name="item[images_attributes][${num}][src]"
-                    id="item_images_attributes_${num}_src"><br>
-                    <div class="js-remove">削除</div>
+                    id="item_images_attributes_${num}_src">
+                    <span class="js-remove">削除</span>
                   </div>`;
     return html;
   }
