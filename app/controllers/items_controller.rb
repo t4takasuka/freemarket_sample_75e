161cf257d46da33a17c96650868af516b17cf45d
teto72 <tsubasa.k.729@gmail.com>
2020-06-04 16:24:03 +0900

    update:カテゴリー情報を取得する記述追加
    DBからカテゴリー情報を取得する。
    @category_parent 、@category_children
    @category_grandchildren
    インスタン変数追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_item, except: %i[index new create]
+  before_action :set_item, except: %i[index new create get_category_children get_category_grandchildren]
 
   def index
     @items = Item.all
@@ -9,6 +9,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @item.images.new
+    @category_parent = Category.where(ancestry: nil)
+  end
+
+  def get_category_children
+    @category_children = Category.find("#{params[:parent_name]}").children
+  end
+
+  def get_category_grandchildren
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
 
   def create
