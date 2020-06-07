8557408cc3f5aaa2a2d00d0476afde3f3d790000
t4takasuka <t4botks@gmail.com>
2020-06-07 12:30:15 +0900

    fix-item-model-size-id

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -41,7 +41,7 @@ class ItemsController < ApplicationController
 
   def create
     @item = Item.new(item_params)
-    if @item.save!
+    if @item.save
       redirect_to root_path, notice: "商品を出品しました"
     else
       @item.images.build
