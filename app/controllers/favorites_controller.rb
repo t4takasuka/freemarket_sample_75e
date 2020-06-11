41a727269278c6f72affbd2c8cf1bf2aa780e005
teto72 <tsubasa.k.729@gmail.com>
2020-06-11 12:13:20 +0900

    add:indexアクション追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,6 +1,10 @@
 class FavoritesController < ApplicationController
   before_action :set_item, only: [:create, :destroy]
 
+  def index 
+    @favorites = Favorite.where(user_id: current_user.id)
+  end
+
   def create
     @favorite = Favorite.new(user_id:current_user.id,item_id: @item.id)
     @favorite.save
